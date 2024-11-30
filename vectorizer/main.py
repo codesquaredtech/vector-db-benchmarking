32e76e7d6e1fb1b0e30dbeaaecf22af5f14fb104
TodorovicNikola <nikola.todorovic@uns.ac.rs>
2024-11-30 18:30:56 +0100

    Vectorizer expanded to write to parquet files

--- vectorizer/main.py
+++ vectorizer/main.py
@@ -1,18 +1,19 @@
+from itertools import chain
 from multiprocessing import Pool
 
+import pandas as pd
 from deepface import DeepFace
 
-from app.db.milvus import milvus_client
 from app.face_detection import (create_embedding,
                                 extract_faces_from_deepface_detections,
-                                initialise_face_detector,
                                 initialise_face_embedder)
 from app.images import convert_bytes_to_image, get_image_paths
 from app.logger import get_logger
 
 SUPPORTED_IMAGE_TYPES = ('.png', '.jpg', '.jpeg', '.bmp', '.gif')
-REFERENT_IMAGE_DIRECTORY = './images/testing/'
-CHUNK_SIZE = 100
+REFERENT_IMAGE_DIRECTORY = './images/NORTHSTORM/2024/'
+OUTPUT_FILE_PATH = './output/embeddings_{current_datetime}.parquet' 
+CHUNK_SIZE = 1
 POOL_PROCESSES = 1
 
 
@@ -20,12 +21,11 @@ def process_image(image_path):
     logger.info(f"Processing image: {image_path}")
 
     vectors_to_insert = []
-    face_detector = initialise_face_detector(is_short_range=True)
     face_embedder = initialise_face_embedder()
 
-    numpy_image, mediapipe_image = convert_bytes_to_image(image_path)
+    numpy_image, _ = convert_bytes_to_image(image_path)
     detected_faces = DeepFace.extract_faces(
-                                img_path=numpy_image,
+                                img_path=numpy_image, enforce_detection=False
                             )
     face_images = extract_faces_from_deepface_detections(
         detected_faces
@@ -40,15 +40,20 @@ def process_image(image_path):
             }
         )
     
-    milvus_client.insert("faces_from_events", vectors_to_insert)
+    return vectors_to_insert
 
 
-def process_images_in_directory(directory_path, chunk_size=100):
+def process_images_in_directory(directory_path, current_datetime, chunk_size=100):
     image_files = get_image_paths(directory_path=directory_path, supported_image_types=SUPPORTED_IMAGE_TYPES)
     logger.info(f"len of image_files: {len(image_files)}")
 
     with Pool(processes=POOL_PROCESSES) as pool:
-        pool.map(process_image, image_files, chunksize=chunk_size)
+        list_of_lists = pool.map(process_image, image_files, chunksize=chunk_size)
+        flattened_list = list(chain.from_iterable(list_of_lists))
+        # logger.info(f"Flattened list: {flattened_list}")
+
+        df = pd.DataFrame(flattened_list)
+        df.to_parquet(OUTPUT_FILE_PATH.format(current_datetime=current_datetime.strftime("%Y-%m-%d_%H-%M-%S")), compression='snappy')
 
 
 def main():    
@@ -56,7 +61,7 @@ def main():
     start_datetime = datetime.datetime.now()
     logger.info(f'Starting vectorizing at: {start_datetime.strftime("%Y-%m-%d_%H-%M-%S")}')
 
-    process_images_in_directory(REFERENT_IMAGE_DIRECTORY, chunk_size=CHUNK_SIZE)
+    process_images_in_directory(REFERENT_IMAGE_DIRECTORY, current_datetime=start_datetime, chunk_size=CHUNK_SIZE)
 
     end_datetime = datetime.datetime.now()
     logger.info(f'Finished vectorizing at: {end_datetime.strftime("%Y-%m-%d_%H-%M-%S")}')
