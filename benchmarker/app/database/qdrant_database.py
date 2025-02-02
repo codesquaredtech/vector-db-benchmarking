2a01c58dddc03d6844d29a539aa689658f031d7a
TodorovicNikola <nikola.todorovic@uns.ac.rs>
2025-02-02 18:11:15 +0100

    Implemented remaining methods

--- benchmarker/app/database/qdrant_database.py
+++ benchmarker/app/database/qdrant_database.py
@@ -4,7 +4,11 @@ from app.logger import get_logger
 logger = get_logger()
 
 from qdrant_client import QdrantClient
-from qdrant_client.models import Distance, HnswConfig, VectorParams
+from qdrant_client.models import (Batch, Distance, FieldCondition, Filter,
+                                  FilterSelector, Range, SearchParams,
+                                  VectorParams)
+
+VECTOR_NAME = "image_vector"
 
 
 class QdrantDatabase(VectorDatabase):
@@ -12,30 +16,113 @@ class QdrantDatabase(VectorDatabase):
         self.client = None
 
     def connect(self, host="qdrant", port="6333"):
-        self.client = QdrantClient(url=f"http://{host}:{port}")
+        logger.info("Initializing QD client")        
+        try:
+            self.client = QdrantClient(url=f"http://{host}:{port}")
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+            raise e
+
 
     def drop_collection(self, collection_name: str):
-        pass
+        logger.info(f"Dropping {collection_name} collection")
+        try:
+            self.client.delete_collection(collection_name)
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+            raise e
     
     def create_collection(self, collection_name: str):
-        self.client.create_collection(
-            collection_name=f"{collection_name}",
-            vectors_config={
-                "dense": VectorParams(size=1280, 
-                                            distance=Distance.COSINE),
-            },
-            hnsw_config={"m": 16, "ef_construct": 200} # Trebalo bi razmisliti i o ostalim potencijalnim konfiguracijama
-        )
+        logger.info(f"Creating {collection_name} collection")
+        try:
+            self.client.create_collection(
+                collection_name=f"{collection_name}",
+                vectors_config={
+                    VECTOR_NAME: VectorParams(size=1280, 
+                                                distance=Distance.COSINE),
+                },
+                hnsw_config={"m": 16, "ef_construct": 200} # Trebalo bi razmisliti i o ostalim potencijalnim konfiguracijama
+            )
+
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+            raise e
 
 
     def insert(self, collection_name: str, data):
-        pass
+        ids = data.index.tolist()
+        embeddings = data["embedding"].tolist()
+        image_paths = data["image_path"].tolist()
+        images_as_payload = [{"image_path": image_path} for image_path in image_paths]
+        
+        logger.info(f"Inserting {len(ids)} points into {collection_name} collection")
+
+        try:
+            self.client.upsert(
+                collection_name=f"{collection_name}",
+                points=Batch(
+                    ids=ids,
+                    payloads=images_as_payload,
+                    vectors= {
+                        VECTOR_NAME: embeddings,
+                    }
+                ),
+            )
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+            raise e
 
     def delete(self, collection_name: str):
-        pass
+        logger.info(f"Deleting everything from {collection_name}")
+
+        try:
+            self.client.delete(
+                collection_name=f"{collection_name}",
+                points_selector=FilterSelector(
+                    filter=Filter(
+                        must=[
+                            FieldCondition(
+                                key="id",
+                                range=Range(
+                                    gt=0,
+                                    gte=None,
+                                    lt=None,
+                                    lte=None,
+                                ),
+                            ),
+                        ],
+                    )
+                ),
+            )
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+            raise e
+
 
     def search(self, collection_name: str, embedding: list, params: dict):
-       pass
+        logger.info(f"Searching in collection {collection_name}")
+        response = []
+        try:
+            response = self.client.query_points(
+                collection_name=f"{collection_name}",
+                query=embedding,
+                search_params=SearchParams(exact=False),
+                score_threshold=params.get('certainty', 0),
+                limit=params.get('limit', 1600),
+                with_payload=["image_path"],
+                using=VECTOR_NAME
+            )
+
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+            raise e
+
+        return response
 
     def parse_search_results(self, results: list):
-        pass
+        logger.info(f"Parsing search results: {results}")
+        similar_embeddings = []
+        for point in results.points:
+            logger.info(f"ID: {point.id}, Image path: {point.payload['image_path']}, Score: {point.score}")
+            similar_embeddings.append(point.payload['image_path'].split("/")[-1])
+        return similar_embeddings
